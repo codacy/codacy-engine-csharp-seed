94b9d1199277c3e4df20bbb6d649acc1cbc8e7ec
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-02-04 12:05:43 +0100

    clean: Use new TIMEOUT_SECONDS env variable

--- src/TimeSpanHelper.cs
+++ src/TimeSpanHelper.cs
@@ -9,51 +9,15 @@ namespace Codacy.Engine.Seed
     public static class TimeSpanHelper
     {
         /// <summary>
-        ///     Parse a TimeSpan from a string formatted with ('number.time_scale' or 'number time_scale')
-        ///     Available time scales:
-        ///       - second(s)
-        ///       - minute(s)
-        ///       - hour(s)
-        ///
-        ///     e.g.: '1 hour', '2 hours', '30.minutes'
+        ///     Parse a TimeSpan from a string that contains a integer
+        ///     with seconds
         /// </summary>
         /// <param name="value"></param>
         /// <returns></returns>
         /// <exception cref="FormatException"></exception>
         public static TimeSpan Parse(string value)
         {
-            var timeSplitted = value.Replace('.', ' ').Split(' ');
-            int timeFactor;
-
-            if (timeSplitted.Length > 1)
-            {
-                switch (timeSplitted[1])
-                {
-                    case "second":
-                    case "seconds":
-                        timeFactor = 1;
-                        break;
-
-                    case "minute":
-                    case "minutes":
-                        timeFactor = 60;
-                        break;
-
-                    case "hour":
-                    case "hours":
-                        timeFactor = 3600;
-                        break;
-
-                    default:
-                        throw new FormatException();
-                }
-            }
-            else
-            {
-                timeFactor = 1;
-            }
-
-            return TimeSpan.FromSeconds(int.Parse(timeSplitted[0]) * timeFactor);
+            return TimeSpan.FromSeconds(int.Parse(value));
         }
     }
 }
--- test/Codacy.Engine.Seed.Tests/TimeSpanTests.cs
+++ test/Codacy.Engine.Seed.Tests/TimeSpanTests.cs
@@ -9,25 +9,12 @@ namespace Codacy.Engine.Seed.Tests
         [Fact]
         public void ParseTest()
         {
-            TimeSpan time = TimeSpanHelper.Parse("60 seconds");
+            TimeSpan time = TimeSpanHelper.Parse("60");
             Assert.Equal(new TimeSpan(0,0,0,60), time);
-
-            time = TimeSpanHelper.Parse("60 second");
-            Assert.Equal(new TimeSpan(0,0,0,60), time);
-
-            time = TimeSpanHelper.Parse("1 hour");
-            Assert.Equal(new TimeSpan(0,1,0,0), time);
-
-            time = TimeSpanHelper.Parse("2.minutes");
-            Assert.Equal(new TimeSpan(0,0,2,0), time);
-
-            time = TimeSpanHelper.Parse("2");
-            Assert.Equal(new TimeSpan(0,0,0,2), time);
 
             Assert.Throws<FormatException>(() => {
-                TimeSpanHelper.Parse("60.secondz");
+                TimeSpanHelper.Parse("bla bla");
             });
-
         }
     }
 }
